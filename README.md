bafce06948bd542f6c7560787ce43a1a31632650
Gongagaga <gwendal.nog@gmail.com>
2023-04-28 11:52:31 +0200

    tp calculatrice fonctionnelle

--- README.md
+++ README.md
@@ -1,70 +1,30 @@
-# Getting Started with Create React App
+# Calculatrice avec bouton
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Vous allez faire une petite calculatrice en utilisant **useReducer**.
 
-## Available Scripts
+Créez un champ de saisi fictif et 10 boutons de 0 à 9 pour afficher dans la partie "saisie" le nombre tapé. Créez les trois boutons suivants :
 
-In the project directory, you can run:
+1. Un bouton + pour effectuer l'addition.
 
-### `npm start`
+2. Un bouton - pour effectuer la soustraction.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+3. Un bouton X pour faire la multiplication.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Implémentez également les boutons proposés dans le Wireframe ci-dessous.
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+```text
+[ 9182 ]  <-- champ texte
 
-### `npm run build`
+[7] [8] [9]   <-- Bouton cliquable pour afficher un nombre 
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+[4] [5] [6]
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+[1] [2] [3]
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+[0]
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[+] [-] [*] <-- Lorsque vous cliquez sur un bouton de type opération alors le champ de saisi s'efface
+[=]
+[Reset]
+```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
 
 function App() {
+  const [state, dispatch] = useReducer(
+    //definition du const state
+    (state, action) => {
+      console.log(action);
+
+      //switch pour les différents cas d'action ( gestions des boutons cliqué )
+      switch (action.type) {
+        case "Enter_number":
+          return {
+            ...state, // copie/mise a jour d'une seule propriété de l'objet state
+            currentNumber: state.currentNumber + action.index,
+          };
+        case "Set_operator":
+          return {
+            currentNumber: "",
+            storedNumber: state.currentNumber,
+            operator: action.index,
+          };
+        //cas où on additionne toutes les données rentrées (storedNumber operator currentNumber)
+        //ex : 12(storedNumber) +(operator) 8(currentNumber)
+        case "Calculate":
+          const current = parseFloat(state.currentNumber);
+          const stored = parseFloat(state.storedNumber);
+          switch (state.operator) {
+            case "+":
+              return {
+                ...state, // copie/mise a jour d'une seule propriété de l'objet state
+                currentNumber: stored + current, // addition des nombres stockés et actuels
+                storedNumber: "", // suppression du nombre stocké apres calcul
+              };
+            case "-":
+              return {
+                ...state,
+                currentNumber: stored - current,
+                storedNumber: "",
+              };
+            case "*":
+              return {
+                ...state,
+                currentNumber: stored * current,
+                storedNumber: "",
+              };
+            default:
+              return state;
+          }
+        case "Reset":
+          return { currentNumber: "", storedNumber: "", operator: "" };
+        default:
+          return state;
+      }
+    },
+    //definition du const dispatch
+    {
+      currentNumber: "", //le nombre actuel
+      storedNumber: "", //ici on stock
+      operator: "", // le signe d'opération choisis
+    }
+  );
+
+  const handleNumberClick = (number) => {
+    dispatch({ type: "Enter_number", index: number });
+  };
+
+  const handleOperatorClick = (operator) => {
+    dispatch({ type: "Set_operator", index: operator });
+  };
+
+  const handleCalculateClick = () => {
+    dispatch({ type: "Calculate" });
+  };
+
+  const handleResetClick = () => {
+    dispatch({ type: "Reset" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={state.currentNumber} readOnly></input>
+      <div className="topRow">
+        <button onClick={() => handleNumberClick("7")}>7</button>
+        <button onClick={() => handleNumberClick("8")}>8</button>
+        <button onClick={() => handleNumberClick("9")}>9</button>
+      </div>
+      <div className="midRow">
+        <button onClick={() => handleNumberClick("4")}>4</button>
+        <button onClick={() => handleNumberClick("5")}>5</button>
+        <button onClick={() => handleNumberClick("6")}>6</button>
+      </div>
+      <div className="bottomRow">
+        <button onClick={() => handleNumberClick("1")}>1</button>
+        <button onClick={() => handleNumberClick("2")}>2</button>
+        <button onClick={() => handleNumberClick("3")}>3</button>
+      </div>
+      <div className="zeroRow">
+        <button onClick={() => handleNumberClick("0")}>0</button>
+      </div>
+      <div className="operatorRow">
+        <button onClick={() => handleOperatorClick("+")}>+</button>
+        <button onClick={() => handleOperatorClick("-")}>-</button>
+        <button onClick={() => handleOperatorClick("*")}>x</button>
+      </div>
+      <div className="equalRow">
+        <button onClick={handleCalculateClick}>=</button>
+        <button onClick={handleResetClick}>Reset</button>
+      </div>
     </div>
   );
 }
